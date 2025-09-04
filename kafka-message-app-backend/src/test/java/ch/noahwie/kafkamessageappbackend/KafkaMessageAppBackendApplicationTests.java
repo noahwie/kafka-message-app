ab5b051ebf299ee6c66de2a71262d1e3abb547c7
Noah WIeschmann <124290589+noahwie@users.noreply.github.com>
2025-09-04 10:21:00 +0200

    backend initital project structure

---
+++ kafka-message-app-backend/src/test/java/ch/noahwie/kafkamessageappbackend/KafkaMessageAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noahwie.kafkamessageappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMessageAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
