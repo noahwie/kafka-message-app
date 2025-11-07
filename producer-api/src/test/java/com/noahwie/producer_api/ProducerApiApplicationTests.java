d7a34ad231f3d4ece6ec002938dc768def9bcff0
Noah WIeschmann <124290589+noahwie@users.noreply.github.com>
2025-11-07 11:25:47 +0100

    created basic project structure

---
+++ producer-api/src/test/java/com/noahwie/producer_api/ProducerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noahwie.producer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
